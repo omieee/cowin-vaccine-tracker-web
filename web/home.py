a53271201ee959de7a4c09b49f7ab97dec278f0b
om-shanker <om.shanker@bostonharborconsulting.com>
2021-05-19 17:22:56 +0530

    Upadted

--- web/home.py
+++ web/home.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='template')
+
 
 @app.route('/')
 def home():
-   return render_template('../web/static/index.html')
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
